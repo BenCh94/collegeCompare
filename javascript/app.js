d52a5075a5d409fcc201985d7e3d20ef09960997
Ben Chadwick <bchadwick94@gmail.com>
2016-06-09 12:33:08 +0100

    fixed broken carousel

--- javascript/app.js
+++ javascript/app.js
@@ -1,2 +1,2 @@
-angular.module("collegeCompare",['collegeControllers','ui.bootstrap.demo']);
-angular.module('ui.bootstrap.demo', ['ngAnimate', 'ui.bootstrap']);
+angular.module("collegeCompare",['collegeControllers','ui.bootstrap']);
+
--- javascript/controller.js
+++ javascript/controller.js
@@ -1,4 +1,5 @@
-angular.module('collegeControllers',[]).controller('MyController',function($scope) {
+angular.module('collegeControllers',[])
+    .controller('MyController',function($scope) {
 //	controller	logic	here
     $scope.showInfo = function (infoId) {
         $(infoId).show();
@@ -11,64 +12,15 @@ angular.module('collegeControllers',[]).controller('MyController',function($scop
     };
 //Trying to make twitter api work in angular this code from ngtweet website
     //var myApp = angular.module("collegeCompare", ['ngtweet']);
-});
-/*angular.module('ui.bootstrap', []).controller('ModalCtrl',function($scope, $uibmodal){
+})
+angular.module('ui.bootstrap', [])
+    .controller('ModalCtrl',function($scope, $uibmodal){
     $scope.open = function (size) {
         $uibModal.open();
     };
-});*/
+})
 
-angular.module('ui.bootstrap.demo').controller('ModalDemoCtrl', function ($scope, $uibModal, $log) {
 
-  $scope.items = ['item1', 'item2', 'item3'];
-
-  $scope.animationsEnabled = true;
-
-  $scope.open = function (size) {
-
-    var modalInstance = $uibModal.open({
-      animation: $scope.animationsEnabled,
-      templateUrl: 'myModalContent.html',
-      controller: 'ModalInstanceCtrl',
-      size: size,
-      resolve: {
-        items: function () {
-          return $scope.items;
-        }
-      }
-    });
-
-    modalInstance.result.then(function (selectedItem) {
-      $scope.selected = selectedItem;
-    }, function () {
-      $log.info('Modal dismissed at: ' + new Date());
-    });
-  };
-
-  $scope.toggleAnimation = function () {
-    $scope.animationsEnabled = !$scope.animationsEnabled;
-  };
-
-});
-
-// Please note that $uibModalInstance represents a modal window (instance) dependency.
-// It is not the same as the $uibModal service used above.
-
-angular.module('ui.bootstrap.demo').controller('ModalInstanceCtrl', function ($scope, $uibModalInstance, items) {
-
-  $scope.items = items;
-  $scope.selected = {
-    item: $scope.items[0]
-  };
-
-  $scope.ok = function () {
-    $uibModalInstance.close($scope.selected.item);
-  };
-
-  $scope.cancel = function () {
-    $uibModalInstance.dismiss('cancel');
-  };
-});
 
 
 
